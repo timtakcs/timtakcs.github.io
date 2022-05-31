f0e47f9221a87574ef0ad544036b236128cb33d7
timtakcs <timur_takhtarov@houston.nae.school>
2022-05-31 10:02:36 -0500

    sorry nirav

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 function autoScroll() {
-    var element = document.getElementById("projects");
+    const element = document.getElementById("projects");
     element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 }
 
