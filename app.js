77c93702ff0b0a8943b6ae5f9f429000b4758bfb
timtakcs <timur_takhtarov@houston.nae.school>
2022-05-31 22:45:21 -0500

    fjdksafjdklas;

--- app.js
+++ app.js
@@ -4,4 +4,23 @@ function autoScroll() {
     element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
 }
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.lengthl; i++) {
+        var windowHeight = widner.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active")
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
 
